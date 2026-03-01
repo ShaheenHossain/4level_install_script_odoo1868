4ece92cd96fcb6accb3d00d648c36e7ef809b554
ShaheenHossain <rapidgrps@princegroup-bd.com>
2026-03-01 15:58:36 +0600

    Update odoo1868_install.sh

--- odoo1868_install.sh
+++ odoo1868_install.sh
@@ -66,8 +66,8 @@ sudo apt-get install git python3-cffi build-essential wget python3-dev python3-v
 echo -e "\n---- Install python packages/requirements ----"
 
 #sudo -H pip3 install -r https://github.com/ShaheenHossain/requirements.txt/raw/master/requirements.txt
-
-sudo -H pip3 install -r https://github.com/odoo/odoo/raw/17.0/requirements.txt
+sudo -H pip3 install -r https://github.com/odoo/odoo/raw/18.0/requirements.txt --break-system-packages
+#sudo -H pip3 install -r https://github.com/odoo/odoo/raw/17.0/requirements.txt
 
 echo -e "\n---- Installing nodeJS NPM and rtlcss for LTR support ----"
 sudo apt-get install nodejs npm -y
